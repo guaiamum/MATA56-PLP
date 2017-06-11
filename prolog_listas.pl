2e4576df76002837b2c4adbd2b7bfe5d4cf1887d
Guaiamum <cainabrazil@gmail.com>
2017-06-11 11:12:30 -0300

    teste update upstream

--- prolog_listas.pl
+++ prolog_listas.pl
@@ -1,13 +1,13 @@
-%retorna true se todos os elementos da lista L s„o o ·tomo 'a'
+%retorna true se todos os elementos da lista L s√£o o √°tomo 'a'
 todos_as(L) :-
     L = [].
 todos_as([Head | Tail]) :-
     Head = a,
     todos_as(Tail).
- 
+
  % Soma de todos os elementos da lista.
 soma([],0).
-soma([H|T],S) :- 
+soma([H|T],S) :-
     soma(T,G),
     S is H+G.
 
