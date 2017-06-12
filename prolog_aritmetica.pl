c4fd4b639af7f050ae9c61f00f021edb0f68a5a0
Cainã Brazil <cainabrazil@gmail.com>
2017-06-11 23:07:07 -0300

    29 and 30 not ready

--- prolog_aritmetica.pl
+++ prolog_aritmetica.pl
@@ -9,3 +9,32 @@
 1 + 2 =:= 2 + X. %false / NOT sufficiently instantiated
 X is 1 + 2. % X = 3
 1 + 2 is X. % X = 1 + 2 / NOT sufficiently instantiated
+
+% entre/3 : indica se o número X está entre os números A e B
+entre(X, A, B) :- 
+    X >= A,
+    X =< B.
+
+%triplo/2 : indica se X é o triplo do número Y
+triplo(X, Y) :-
+    X is (3 * Y).
+    
+%par/1 : que indica se o número inteiro não-negativo X é par
+par(X) :-
+    X >= 0,
+    0 is mod(X,2).
+
+%parNoMod/1 : reescrever regra anterior sem mod(X,Y)
+ehpar(X) :-
+    X > 1,
+    ehpar(div(X, 2)).
+ehpar(X) :-
+    1 is X.
+    
+%fatorial/2 : indica se o fatorial de X é Y. 
+fatorial(X,Res) :-
+    0 = X,
+    Res = 1.
+fatorial(X,Res) :-
+    X >= 1,
+    fatorial(X-1, Res).
